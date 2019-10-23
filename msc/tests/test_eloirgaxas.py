21188b6e32677aa69e88ba4b6d1056f4106a8b28
Eloi Rodriguez Gaxas <eloirodriguezgaxas@Elois-MacBook-Pro.local>
2019-10-23 11:40:56 +0100

    added a second test

--- msc/tests/test_eloirgaxas.py
+++ msc/tests/test_eloirgaxas.py
@@ -2,3 +2,6 @@ from msc.rot13 import rot13, rot13_char
 
 def test_rot13_char_a():
     assert "n" == rot13_char("a"), "Unexpected character"
+
+def test_rot13_not_char():
+    assert "2" == rot13_char("2"), "Should awnser 2"
